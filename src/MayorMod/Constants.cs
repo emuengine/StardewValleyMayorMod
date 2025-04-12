2fb225755b30eb56afa20d3b118f5228847a9720
emuengine <github.qvxqs@passmail.com>
2025-04-12 21:23:33 +0200

    Fixes and cleanup - More cleanup for dialogue - Fix for SMAPI voting interactions - Fixing localisation for dialogue

--- src/MayorMod/Constants.cs
+++ src/MayorMod/Constants.cs
@@ -1,16 +1,17 @@
 ﻿namespace MayorMod;
 public static class Constants
 {
-    public const string MayorModPrefix = "MayorMod";
-    public readonly static string MayorModSaveKey = $"{MayorModPrefix}_SaveData";
+    public static readonly string  MayorModPrefix = "MayorMod";
+    public static readonly string  MayorModCPPrefix = "EmuEngine.MayorModCP";
+    public static readonly string  MayorModSaveKey = $"{MayorModPrefix}_SaveData";
 
     public static class ProgressKey
     {
-        public readonly static string RegisteringForBalot = $"{MayorModPrefix}_EnableRegistrationOffice";
-        public readonly static string VotingMayor = $"{MayorModPrefix}_EnableVotingBooth";
-        public readonly static string RunningForMayor = $"{MayorModPrefix}_RunningForMayor";
-        public readonly static string VotedForMayor = $"{MayorModPrefix}_HasVoted";
-        public readonly static string ElectedAsMayor = $"{MayorModPrefix}_ElectedAsMayor";
+        public static readonly string  RegisteringForBalot = $"{MayorModPrefix}_EnableRegistrationOffice";
+        public static readonly string  VotingMayor = $"{MayorModPrefix}_EnableVotingBooth";
+        public static readonly string  RunningForMayor = $"{MayorModPrefix}_RunningForMayor";
+        public static readonly string  VotedForMayor = $"{MayorModPrefix}_HasVoted";
+        public static readonly string  ElectedAsMayor = $"{MayorModPrefix}_ElectedAsMayor";
     }
     public static class ActionKey
     {
@@ -22,22 +23,23 @@ public static class Constants
     }
     public static class ItemKey
     {
-        public const string Ballot = "MayorMod_Ballot";
-        public const string BallotUsed = "MayorMod_BallotUsed";
-        public const string ElectionSign = "MayorMod_ElectionSign";
+
+        public static readonly string Ballot = $"{MayorModCPPrefix}_Ballot";
+        public static readonly string  BallotUsed = $"{MayorModCPPrefix}_BallotUsed";
+        public static readonly string  ElectionSign = $"{MayorModCPPrefix}_ElectionSign";
     }
     public static class DialogueKey
     {
-        public const string RegisterForBallot = "Strings\\Characters:RegisterForBallot";
-        public const string GetBallot = "Strings\\Characters:GetBallot";
-        public const string NeedToFillBallot = "Strings\\Characters:NeedToFillBallot";
-        public const string HaveVoted = "Strings\\Characters:HaveVoted";
-        public const string CantCarryBallot = "Strings\\Characters:CantCarryBallot";
-        public const string NeedBallot = "Strings\\Characters:NeedBallot";
-        public const string NeedToVote = "Strings\\Characters:NeedToVote";
+        public static readonly string  RegisterForBallot = "Characters/Dialogue/MayorMod_OfficerMike:RegisterForBallot";
+        public static readonly string  GetBallot = "Characters/Dialogue/MayorMod_OfficerMike:GetBallot";
+        public static readonly string  NeedToFillBallot = "Characters/Dialogue/MayorMod_OfficerMike:NeedToFillBallot";
+        public static readonly string  HaveVoted = "Characters/Dialogue/MayorMod_OfficerMike:HaveVoted";
+        public static readonly string  CantCarryBallot = "Characters/Dialogue/MayorMod_OfficerMike:CantCarryBallot";
+        public static readonly string  NeedBallot = "Characters/Dialogue/MayorMod_OfficerMike:NeedBallot";
+        public static readonly string  NeedToVote = "Characters/Dialogue/MayorMod_OfficerMike:NeedToVote";
     }
     public static class AssetPath
     {
-        public const string BallotTexturePath = "LooseSprites/Ballot";
+        public static readonly string  BallotTexturePath = $"Mods/{MayorModCPPrefix}/Ballot";
     }
 }
